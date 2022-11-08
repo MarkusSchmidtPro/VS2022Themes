1a104d352ff05ef2262782e0b446f35b1800fc2f
Markus Schmidt <markus@markusschmidt.pro>
2022-11-08 09:56:03 +0100

    First Commit

---
+++ src/MSPro.Themes/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Markus Schmidt's PRO VS2022 Themes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Markus Schmidt .PRO")]
+[assembly: AssemblyProduct("MSPro.Themes.VS2022.Properties")]
+[assembly: AssemblyCopyright("Copyright (c) 2022, Markus Schmidt (PRO), all rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93b774be-b169-4173-a487-250ebc294414")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
